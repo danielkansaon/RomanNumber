da0723db0e496b71b07cd2413667ff4911eeea48
Flavio Pedrosa <flaviopedrosa@gmail.com>
2019-03-07 20:17:05 -0300

    Teste de Numero romano para decimal

---
+++ RomanNumber/App.Test/RomanNumberTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Test
+{
+    [TestClass]
+    public class RomanNumberTest
+    {
+        [TestMethod]
+        public void RomanoParaDecimal()
+        {
+            // converter xx para numero decimal
+            int numeroEsperado = 20;
+            string numeroRomano = "XX";
+            int res = App.Romano.ToDecimal("XX");
+            Assert.AreEqual(numeroEsperado, res);
+        }
+    }
+}
