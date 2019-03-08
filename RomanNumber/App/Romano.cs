028abc4a2f1c469220df93d96e55ec2fc9b80e49
Juliana Lobato da Silva <juliana.lobato@mrv.com.br>
2019-03-07 21:28:46 -0300

    criação do metodo ToDecimal

---
+++ RomanNumber/App/Romano.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App
+{
+    public class Romano
+    {
+        Dictionary<string, int> _numerosRomanos = new Dictionary<string, int>()
+        {
+            {"I", 1},
+            {"V", 5},
+            {"X", 10},
+            {"L", 50},
+            {"C", 100},
+            {"D", 500},
+            {"M", 1000}
+        };
+
+        public int ToDecimal(string numeroRomano)
+        {
+            var algarismos = new List<string>(numeroRomano.Split(""));
+            int retorno = 0;
+
+            foreach(var al in algarismos)
+            {
+                retorno +=_numerosRomanos[al];
+            }
+
+            return retorno;
+        }
+    }
+}
